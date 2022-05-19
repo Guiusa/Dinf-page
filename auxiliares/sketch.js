3c98bebbd796898b51dd019f8f90ff859c42f48f
guiusa <guiusepeoneda@gmail.com>
2022-05-18 22:38:35 -0300

    testing some CSS-p5 iteractions

--- auxiliares/sketch.js
+++ auxiliares/sketch.js
@@ -1,13 +1,37 @@
-var i = 0;
-
+var toruses = [];
+var ang = 0;
 function setup() {
-  createCanvas(500, 500);
-  fill(0, 255, 0);
+  angleMode(DEGREES);
+  createCanvas(1080, 1080, WEBGL);
   noStroke();
+  
+  for(var i=0; i<10; i++){
+    var torus = [];
+    torus[0] = random(0, 360);
+    torus[1] = random(0, 360);
+    torus[2] = random(0, 360);
+    toruses[i] = torus;
+  }
+
+  fill(0);
+  ellipsoid(150);
+  fill(255, 255, 100);
 }
 
 function draw() {
   background(0);
-  ellipse(250, 250, cos(i/100)*500, cos(i/100)*500);
-  i++;
+  rotateX(ang);
+  rotateZ(ang/2);
+  for(var i=0; i<10; i++){
+    push();
+    rotateX(toruses[i][0]);
+    rotateY(toruses[i][1]);
+    rotateZ(toruses[i][2]);
+    torus(500, 3);
+    pop();
+  }
+  fill(0);
+  ellipsoid(150);
+  fill(255, 255, 100);
+  ang++;
 }
