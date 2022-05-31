acf03d812d22c32f3b2418cb7e154f9509da9e18
godp21 <godp21@inf.ufpr.br>
2022-05-31 10:16:56 -0300

    managed to make multiple p5 scripts run at same time and interact via CSS. Epic dev moment

---
+++ auxiliares/azul.js
@@ -0,0 +1,8 @@
+new p5(function (k) {
+    k.setup = function () {
+        k.createCanvas(1080, 1080);
+        k.background(0, 0, 255);
+    };
+    k.draw = function () {
+    };
+}, "azul");
---
+++ auxiliares/verde.js
@@ -0,0 +1,8 @@
+new p5(function (k) {
+    k.setup = function () {
+        k.createCanvas(1080, 1080);
+        k.background(0, 255, 0);
+    };
+    k.draw = function () {
+    };
+}, "verde");
